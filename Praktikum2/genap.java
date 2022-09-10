2d7a9198a31c513d6f723ba7af65555f0e24d485
Teuku Rifal Aulia <60639687+rifal-droid@users.noreply.github.com>
2022-09-10 10:07:18 +0700

    Update genap.java

--- Praktikum2/genap.java
+++ Praktikum2/genap.java
@@ -1,8 +1,11 @@
+/* program menampilkan bilangan genap
+dari angka 1 sampai 20 */
+
 public class genap{
     public static void main(String[] args){
 
     for(int i=1; i <= 20; i++){
-        if(i % 2 == 1){ //jika i habis dibagi 2 dan hasilnya 1 maka akan di skip.
+        if(i % 2 == 1){ //jika i habis dibagi 2 dan sisa baginya 1 maka akan di skip. jadi yang ditampilkan hanya angka genap.
             continue; 
         }
     System.out.println(i);
