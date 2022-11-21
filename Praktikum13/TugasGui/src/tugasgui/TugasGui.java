89a5581304481936e6812eba8c10769c584a06b6
Teuku Rifal Aulia <60639687+rifal-droid@users.noreply.github.com>
2022-11-21 16:23:40 +0700

    Add files via upload

---
+++ Praktikum13/TugasGui/src/tugasgui/TugasGui.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasgui;
+
+/**
+ *
+ * @author lenovo
+ */
+public class TugasGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
